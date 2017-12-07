11e7317e64c59decffd978b7383b19d74a0ddad3
Unknown <t.frankowski7@gmail.com>
2017-12-07 11:26:38 +0100

    fixed2

--- app/src/main/java/it/polimi/deib/p2pchat/discovery/BitmapConverters/StringToBitmapConverter.java
+++ app/src/main/java/it/polimi/deib/p2pchat/discovery/BitmapConverters/StringToBitmapConverter.java
@@ -16,30 +16,12 @@ public class StringToBitmapConverter {
         try {
             byte[] encodeByte = Base64.decode(strBitmap, Base64.DEFAULT);
 
-            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0,
-                    encodeByte.length);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
             Bitmap large = Scaler.getResizedBitmap(bitmap, width, height);
             return large;
         } catch (Exception e) {
             e.getMessage();
             return null;
         }
-    }
-
-    private static Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
-        int width = bm.getWidth();
-        int height = bm.getHeight();
-        float scaleWidth = ((float) newWidth) / width;
-        float scaleHeight = ((float) newHeight) / height;
-        // CREATE A MATRIX FOR THE MANIPULATION
-        Matrix matrix = new Matrix();
-        // RESIZE THE BIT MAP
-        matrix.postScale(scaleWidth, scaleHeight);
-
-        // "RECREATE" THE NEW BITMAP
-        Bitmap resizedBitmap = Bitmap.createBitmap(
-                bm, 0, 0, width, height, matrix, false);
-        bm.recycle();
-        return resizedBitmap;
     }
 }
